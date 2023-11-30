a6d8fbb077c977ccc1fb970c66e8fa3e796b3377
AlejandroAburto <149425227+AlejandroAburto@users.noreply.github.com>
2023-11-30 17:45:37 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.text('Omega 3 & Rocket Smoothie')
+streamlit.text('Kele, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
